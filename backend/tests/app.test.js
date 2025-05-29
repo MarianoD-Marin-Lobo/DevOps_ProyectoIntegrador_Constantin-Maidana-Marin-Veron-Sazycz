bef00b4ba1a806e81698ced64f7e4b74874f094b
MarianoD-Marin-Lobo <mdmarin2@gmail.com>
2025-05-29 19:09:56 -0300

    Actualizacion de Tests

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,10 +1,37 @@
 const request = require('supertest');
-const app = require('../src/app');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const app = require('../app');
+const Todo = require('../models/todo');
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+beforeEach(async () => {
+  await Todo.deleteMany({});
+  await Todo.create([{ title: 'Test 1' }, { title: 'Test 2' }]);
+});
 
 describe('GET /api/todos', () => {
   it('responds with an array', async () => {
     const res = await request(app).get('/api/todos');
     expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBe(2);
   });
 });
