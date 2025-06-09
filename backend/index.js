6ef0600fe3581aa69deb2e0d1e5c5cb01e81865e
MarianoD-Marin-Lobo <mdmarin2@gmail.com>
2025-06-09 20:41:47 -0300

    Modificacion archivos de conexion con MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
+app.use(cors());
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
