44a103efd4dc47df18a4b0ec4e413c0450ba928e
MarianoD-Marin-Lobo <mdmarin2@gmail.com>
2025-06-12 20:38:55 -0300

    Modifiacion archivo app.test

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const app = require('../app');
+const app = require('../tests/app');
 const Todo = require('../models/todo');
 
 let mongoServer;
