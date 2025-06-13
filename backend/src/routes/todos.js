9ab5ecc2d399629d776817a5c268605e126c2d59
MarianoD-Marin-Lobo <mdmarin2@gmail.com>
2025-06-12 21:13:59 -0300

    Modificacion de archivos varios para evitar conexiones innecesarias durante los Tests

--- backend/src/routes/todos.js
+++ backend/src/routes/todos.js
@@ -1,26 +1,58 @@
+// const express = require('express');
+// const Todo = require('../models/Todo');
+// const router = express.Router();
+
+// router.get('/', async (req, res) => {
+//   const todos = await Todo.find();
+//   res.json(todos);
+// });
+
+// router.post('/', async (req, res) => {
+//   const todo = new Todo({ text: req.body.text });
+//   await todo.save();
+//   res.status(201).json(todo);
+// });
+
+// router.put('/:id', async (req, res) => {
+//   const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//   res.json(todo);
+// });
+
+// router.delete('/:id', async (req, res) => {
+//   await Todo.findByIdAndDelete(req.params.id);
+//   res.status(204).end();
+// });
+
+// module.exports = router;
+
+// backend/src/routes/todos.js
 const express = require('express');
-const Todo = require('../models/Todo');
 const router = express.Router();
+const Todo = require('../models/Todo'); // <-- La ruta corregida para importar tu modelo Todo
 
+// Ruta para obtener todos los Todos
 router.get('/', async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
+// Ruta para crear un nuevo Todo
 router.post('/', async (req, res) => {
-  const todo = new Todo({ text: req.body.text });
-  await todo.save();
-  res.status(201).json(todo);
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
-router.put('/:id', async (req, res) => {
-  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(todo);
-});
-
-router.delete('/:id', async (req, res) => {
-  await Todo.findByIdAndDelete(req.params.id);
-  res.status(204).end();
-});
+// ... otras rutas (PUT, DELETE, etc.)
 
 module.exports = router;
